d6afcde6c2ce101f40ae821c153e59dcb96ab7c0
arzoo19goyal <56269320+arzoo19goyal@users.noreply.github.com>
2021-02-18 01:34:25 +0530

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -1,12 +1,14 @@
 // Cloud Code entry point
 Parse.Cloud.define('hello', req => {
-    req.log.info(req);
+//     req.log.info(req);
+    console.log(req);
     return 'Hi';
   });
   
   Parse.Cloud.define('asyncFunction', async req => {
     await new Promise(resolve => setTimeout(resolve, 1000));
-    req.log.info(req);
+//     req.log.info(req);
+    console.log(req);
     return 'Hi async';
   });
   
